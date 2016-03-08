99bb25a508dde4544c5eee8dbc20f3e8286b8212
Skitelman <sghitelman@gmail.com>
2016-03-08 11:02:53 -0500

    models working

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,71 @@
 class Listing < ActiveRecord::Base
+
+  def self.parse_address(raw_address)
+    raw_address.split("#").first.strip
+  end
+
+  def self.parse_unit(raw_address)
+    raw_address.split("#").last unless raw_address.split("#").size < 2
+  end
+
+  def self.parse_url(raw_url)
+    base_url = raw_url.split("?").first
+    "https://www.streeteasy.com#{base_url}"
+  end
+
+  def self.parse_price(raw_price)
+    raw_price.gsub("$", "").gsub(",","").to_i
+  end
+
+  # def attributes
+  #   {
+  #     'listing_class': self.listing_class,
+  #     'address': self.address,
+  #     'unit': self.unit,
+  #     'url': self.url,
+  #     'price': self.price
+  #   }
+  # end
+
+  def safe_save
+    unless Listing.find_by(address: self.address, unit: self.unit, listing_class: self.listing_class)
+      self.save
+    end
+  end
+
+  # def self.all
+  #   @@all
+  # end
+
+  def self.sales
+    self.all.select{|listing| listing.listing_class == "Sale"}
+  end
+
+  def self.rentals
+    self.all.select{|listing| listing.listing_class == "Rental"}
+  end
+
+  def self.most_expensive_sales
+    self.sales.sort_by{|listing|
+      listing.price
+    }.reverse[0...20]
+  end
+
+  def self.most_expensive_rentals
+    self.rentals.sort_by{|listing|
+      listing.price
+    }.reverse[0...20]
+  end
+
+  def self.most_expensive_sales_attributes
+    self.most_expensive_sales.map{|listing|
+      listing.attributes
+    }
+  end
+
+  def self.most_expensive_rentals_attributes
+    self.most_expensive_rentals.map{|listing|
+      listing.attributes
+    }
+  end
 end
--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -8,13 +8,17 @@ class Scraper
   def get_addresses
     self.page.css("div.item div.details-title a").each_with_index do |details, index|
       if index.even?
-        listing = Listing.new
-        listing.parse_url(details["href"])
-        listing.listing_class = details["data-gtm-listing-type"].capitalize
-        listing.parse_address(details.children.text)
-        listing.parse_price(details.parent.parent.css("div.price-info span.price").text)
+        listing = Listing.find_or_create_by(listing_params(details))
+        listing.price = Listing.parse_price(details.parent.parent.css("div.price-info span.price").text)
         listing.save
       end
     end
+  end
+
+  def listing_params(details)
+    {url: Listing.parse_url(details["href"]),
+    listing_class: details["data-gtm-listing-type"].capitalize,
+    address: Listing.parse_address(details.children.text),
+    unit: Listing.parse_unit(details.children.text)}
   end
 end
